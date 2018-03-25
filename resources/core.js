4663ee2893207007220518b5ce8528006a29cd17
DESKTOP-7592QSR\Robby <robertino.aniello@gmail.com>
2018-03-25 18:12:41 +0200

    preview with custom filter

--- resources/core.js
+++ resources/core.js
@@ -3,3 +3,14 @@ var app = angular.module('notes', []);
 app.controller('noteController', ['$scope', function($scope){
     $scope.newNote = "";
 }]);
+
+app.filter('firstUppercase', function(){
+    return function(input){
+        var out = '';
+        input = input || '';
+        if (input.length > 0){
+            out = input.charAt(0).toUpperCase() + input.substring(1);
+        }
+        return out;
+    }
+});
