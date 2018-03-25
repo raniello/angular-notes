1d16822826b5d154fbdeadbe7f91c2fb7ad9af64
DESKTOP-7592QSR\Robby <robertino.aniello@gmail.com>
2018-03-25 18:39:23 +0200

    note list

--- resources/core.js
+++ resources/core.js
@@ -1,16 +1,28 @@
 var app = angular.module('notes', []);
 
 app.controller('noteController', ['$scope', function($scope){
+    $scope.notes = [];
+
     $scope.newNote = "";
+    $scope.addNote = function(note){
+        $scope.notes.push(firstToUpperCase(note));
+        $scope.newNote = "";
+    }
+
+    $scope.deleteNote = function(noteIndex){
+        $scope.notes.splice(noteIndex, 1);
+    }
 }]);
 
 app.filter('firstUppercase', function(){
-    return function(input){
-        var out = '';
-        input = input || '';
-        if (input.length > 0){
-            out = input.charAt(0).toUpperCase() + input.substring(1);
-        }
-        return out;
-    }
+    return firstToUpperCase;
 });
+
+var firstToUpperCase = function(input){
+    var out = '';
+    input = input || '';
+    if (input.length > 0){
+        out = input.charAt(0).toUpperCase() + input.substring(1);
+    }
+    return out;
+};
